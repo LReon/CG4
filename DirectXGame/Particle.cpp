84fb3bd5e61f576fbb7634c80c573621071b9ff3
Rio-Fujiwara <k023g0030@g.neec.ac.jp>
2025-04-17 11:16:09 +0900

    02_02 completed

--- DirectXGame/Particle.cpp
+++ DirectXGame/Particle.cpp
@@ -1,6 +1,8 @@
 #include "Particle.h"
 #include <cassert>
 
+using namespace MathUtility;
+
 // 初期化
 void Particle::Initialize(Model* model) {
 
@@ -10,13 +12,27 @@ void Particle::Initialize(Model* model) {
 	model_ = model;
 	// ワールド変換の初期化
 	worldTransform_.Initialize();
+
+	// 色の設定
+	objectColor_.Initialize();
+	color_ = {1, 1, 0, 1};
+
 }
 
 // 更新
 void Particle::Update() {
 	
-	// 行列を定数バッファに転送
-	worldTransform_.TransferMatrix();
+	// 色変更オブジェクトに色の数値を設定する
+	objectColor_.SetColor(color_);
+
+	// 移動
+	worldTransform_.translation_ += {0.0f, 0.1f, 0.0f};
+
+	//// 行列を定数バッファに転送
+	//worldTransform_.TransferMatrix();
+
+	// 行列を更新
+	worldTransform_.UpdateMatrix();
 
 }
 
@@ -24,6 +40,6 @@ void Particle::Update() {
 void Particle::Draw(Camera& camera) {
 
 	// 3Dモデルの描画
-	model_->Draw(worldTransform_, camera);
+	model_->Draw(worldTransform_, camera,&objectColor_);
 
 }
