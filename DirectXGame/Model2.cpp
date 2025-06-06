5fc6b336ea2b9c165018a6f8d28ec52590e80a3a
Rio-Fujiwara <k023g0030@g.neec.ac.jp>
2025-06-06 11:05:08 +0900

    四角形表示

--- DirectXGame/Model2.cpp
+++ DirectXGame/Model2.cpp
@@ -146,16 +146,16 @@ Model2* Model2::CreateSquare() {
 	indices.resize(kNumIndices);
 
 	// 頂点座標設定
-	vertices[0].pos = {0.0f, 360.0f, 0.0f}; // 左下
+	vertices[0].pos = {-10.0f, -10.0f, 0.0f}; // 左下
 	vertices[0].uv = {0.0f, 1.0f};          // 左下UV
 	vertices[0].normal = {0.0f, 0.0f, 1.0f}; // 上向き法線ベクトル
-	vertices[1].pos = {0.0f, 0.0f, 0.0f};   // 左上
+	vertices[1].pos = {-10.0f, 10.0f, 0.0f};   // 左上
 	vertices[1].uv = {0.0f, 0.0f};           // 左上UV
 	vertices[1].normal = {0.0f, 0.0f, 1.0f}; // 上向き法線ベクトル
-	vertices[2].pos = {640.0f, 360.0f, 0.0f};   // 右下
+	vertices[2].pos = {10.0f, -10.0f, 0.0f};   // 右下
 	vertices[2].uv = {1.0f, 1.0f};           // 右下UV
 	vertices[2].normal = {0.0f, 0.0f, 1.0f}; // 上向き法線ベクトル
-	vertices[3].pos = {640.0f, 0.0f, 0.0f};    // 右上
+	vertices[3].pos = {10.0f, 10.0f, 0.0f};    // 右上
 	vertices[3].uv = {1.0f, 0.0f};           // 右上UV
 	vertices[3].normal = {0.0f, 0.0f, 1.0f}; // 上向き法線ベクトル
 
